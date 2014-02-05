61625be9f6f00836091da0118ffa7132340593f3
Jennifer Vendetti <vendetti@stanford.edu>
2014-02-05 15:48:26 -0800

    fixed formatting

--- register.php
+++ register.php
@@ -4,39 +4,39 @@ $name = $email = $referrer = $url = $affiliation = $description = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") 
 {
-$name = $_POST["name"];
-$email = $_POST["email"];
-$referrer = $_POST["referrer"];
-$url = $_POST["url"];
-$affiliation = $_POST["affiliation"];
-$description = $_POST["project"];
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$referrer = $_POST["referrer"];
+	$url = $_POST["url"];
+	$affiliation = $_POST["affiliation"];
+	$description = $_POST["project"];
 
-// Send an email to the protege-registration mailing list with user registration data.
-$to = 'protege-registration@lists.stanford.edu';
-$subject = 'Protege Registration';
-$headers = array();
-$headers[] = 'Content-type: text/plain; charset=iso-8859-1';
-$headers[] = 'From: protege-registration@lists.stanford.edu';
-$headers[] = 'Reply-To: no-reply@lists.stanford.edu';
-$message = "
+	// Send an email to the protege-registration mailing list with user registration data.
+	$to = 'protege-registration@lists.stanford.edu';
+	$subject = 'Protege Registration';
+	$headers = array();
+	$headers[] = 'Content-type: text/plain; charset=iso-8859-1';
+	$headers[] = 'From: protege-registration@lists.stanford.edu';
+	$headers[] = 'Reply-To: no-reply@lists.stanford.edu';
+	$message = "
 Name: $name
 Email: $email
 Affiliation: $affiliation
 Referred by: $referrer
 Project URL: $url
 Project Description: $description
-";
-mail($to, $subject, $message, implode("\r\n", $headers));
+	";
+	mail($to, $subject, $message, implode("\r\n", $headers));
 
-// Send an email to the registrant, welcoming them to the Protege community.
-unset($to, $subject, $headers, $message);
-$to = $email;
-$subject = 'Welcome to Protege!';
-$headers = array();
-$headers[] = 'Content-type: text/plain; charset=iso-8859-1';
-$headers[] = 'From: protege-registration-request@lists.stanford.edu';
-$headers[] = 'Reply-To: no-reply@lists.stanford.edu';
-$message = '
+	// Send an email to the registrant, welcoming them to the Protege community.
+	unset($to, $subject, $headers, $message);
+	$to = $email;
+	$subject = 'Welcome to Protege!';
+	$headers = array();
+	$headers[] = 'Content-type: text/plain; charset=iso-8859-1';
+	$headers[] = 'From: protege-registration-request@lists.stanford.edu';
+	$headers[] = 'Reply-To: no-reply@lists.stanford.edu';
+	$message = '
 Thanks for registering and welcome to the Protege community!
 
 Need help installing or using Protege? Post your questions on our free user support mailing list.  Use the following link to subscribe:
@@ -65,8 +65,8 @@ We look forward to interacting with you as part of the Protege community!
 
 Best Regards,
 The Protege Team
-';
-mail($to, $subject, $message, implode("\r\n", $headers));
+	';
+	mail($to, $subject, $message, implode("\r\n", $headers));
 }
 
 ?>
