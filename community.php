c0b34460e3149c1ca07c7bae36087c537c727b47
Jennifer Vendetti <vendetti@stanford.edu>
2014-02-07 17:25:04 -0800

    content updates

--- community.php
+++ community.php
@@ -8,7 +8,7 @@
           <h1>
             Thousands of people all over the world are using the 
             Prot&eacute;g&eacute; platform.  Interact with us and 
-            contribute by joining our growing user community.
+            make contributions by joining our active community.
           </h1>
         </div><!-- /end .col-10 -->
       </div><!-- /end .row -->
@@ -40,15 +40,17 @@
     <div class="row">
       <div class="col-4">
         <p class="intro-b">
-          Praesent commodo cursus magna, vel scelerisque nisl 
-          consectetur et. Cras justo odio, dapibus ac facilisis 
-          in, egestas eget quam.
+          There are lots of ways to stay in touch with 
+          Prot&eacute;g&eacute; team members, find users 
+          near you, and make contributions to our project.
         </p>
         <p>
-          Vestibulum id ligula porta felis euismod semper. Nulla
-          vitae elit libero, a pharetra augue. Fusce dapibus, tellus 
-          ac cursus commodo, tortor mauris condimentum nibh, ut fermentum 
-          massa justo sit amet risus.
+          Prot&eacute;g&eacute; is an open source project and we welcome your 
+          involement.  Share your solutions with others on our mailing lists, 
+          sign up for an account on the Prot&eacute;g&eacute; wiki to help us 
+          maintain official documenation, or send us a pull request on 
+          GitHub.  We like to know what you think about Prot&eacute;g&eacute; 
+          and encourage you to stay in touch.
         </p>
         <a href="http://protegewiki.stanford.edu/wiki/Main_Page" class="btn btn-b btn-inline upcase external-link">Visit our Wiki</a>
       </div><!-- /end .col-4 -->
@@ -56,8 +58,7 @@
         <div class="social-panel twitter">
             <div class="pictogram-social twitter-icon-fade"></div>
           <p class="intro-a">
-            Not a developer or don’t have a Facebook acount? Follow us on 
-            Twitter and never miss an update or bug fix again.
+            Not a developer or don’t have a Facebook acount? Follow us on Twitter to get all the latest project updates.
           </p>
           <a href="https://twitter.com/protegeproject" class="twitter-follow-button" data-show-count="false">Follow @protegeproject</a>
           <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
@@ -69,7 +70,7 @@
           <div class="fb-like pull-right" data-href="https://www.facebook.com/pages/Protege-Project/136205016405270" data-width="The pixel width of the plugin" data-height="The pixel height of the plugin" data-colorscheme="light" data-layout="button_count" data-action="like" data-show-faces="true" data-send="false"></div>
           <p class="intro-a">
             Stay up to speed with the latest news regarding the Protégé 
-            Software, team. Support our cause!
+            software and team. Support our cause!
           </p>
         </div><!-- /end .social-panel.facebook -->
       </div><!-- /end .col-4 -->
@@ -79,7 +80,7 @@
           <iframe class="pull-right" src="http://ghbtns.com/github-btn.html?user=protegeproject&type=follow" allowtransparency="true" frameborder="0" scrolling="0" width="165" height="20"></iframe>
           <p class="intro-b">
             Help us with development by forking our repo or reporting bugs 
-            on our Github page. Plus, you’ll be the first to see updates.
+            on our GitHub page. Plus, you’ll be the first to see updates.
           </p>
         </div><!-- /end .social-panel.facebook -->
       </div><!-- /end .col-4 -->
